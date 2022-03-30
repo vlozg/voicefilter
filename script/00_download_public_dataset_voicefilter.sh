dd58d1fe0fc57da1f35f67b6f1788a770b2a0b3b
vlozg <vulong61@gmail.com>
2022-03-30 14:35:25 +0700

    Update script

---
+++ script/00_download_public_dataset_voicefilter.sh
@@ -0,0 +1,32 @@
+apt install axel -y
+cd ../datasets
+
+# Download, then extract LibriSpeech datasets
+(axel -n 10 -q -c "https://www.openslr.org/resources/12/train-clean-100.tar.gz" \
+    && { tar -xzf train-clean-100.tar.gz && rm train-clean-100.tar.gz; }
+    || echo "Download train-clean-100 failed") &
+(axel -n 10 -q -c "https://www.openslr.org/resources/12/train-clean-360.tar.gz" \
+    && { tar -xzf train-clean-360.tar.gz && rm train-clean-360.tar.gz; } \
+    || echo "Download train-clean-360 failed") &
+(axel -n 10 -q -c "https://www.openslr.org/resources/12/dev-clean.tar.gz" \
+    && { tar -xzf dev-clean.tar.gz && rm dev-clean.tar.gz; }
+    || echo "Download dev-clean failed") &
+(axel -n 10 -q -c "https://www.openslr.org/resources/12/test-clean.tar.gz" \
+    && { tar -xzf test-clean.tar.gz && rm test-clean.tar.gz; } \
+    || echo "Download test-clean failed") &
+(axel -n 10 -q -c "https://www.openslr.org/resources/12/dev-other.tar.gz" \
+    && { tar -xzf dev-other.tar.gz && rm dev-other.tar.gz; } \
+    || echo "Download dev-other failed") &
+(axel -n 10 -q -c "https://www.openslr.org/resources/12/test-other.tar.gz" \
+    && { tar -xzf test-other.tar.gz && rm test-other.tar.gz; } \
+    || echo "Download test-other failed") &
+
+# Download VCTK dataset
+mkdir VCTK
+cd VCTK/ 
+(axel -n 10 -q -c "https://datashare.ed.ac.uk/bitstream/handle/10283/3443/VCTK-Corpus-0.92.zip" \
+    && { unzip -q VCTK-Corpus-0.92 && mv wav48_silence_trimmed wav48 && rm VCTK-Corpus-0.92.zip; } \
+    || echo "Download VCTK dataset failed") &
+cd ../
+
+wait
