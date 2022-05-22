7fd4be5fbeb1ab4e6fb7bd247584391d5335b51d
vlozg <vulong61@gmail.com>
2022-05-22 16:17:07 +0700

    Update script to ignore overlapped files

--- script/00_download_VCTK.sh
+++ script/00_download_VCTK.sh
@@ -4,7 +4,7 @@ cd VCTK/
 
 # Download VCTK dataset
 (axel -a -c "https://datashare.ed.ac.uk/bitstream/handle/10283/3443/VCTK-Corpus-0.92.zip?sequence=2&isAllowed=y" \
-    && { unzip -q VCTK-Corpus-0.92 \
+    && { unzip -q -n VCTK-Corpus-0.92 \
     && mv wav48_silence_trimmed wav48 \
     && rm VCTK-Corpus-0.92.zip; } \
     || echo "Download VCTK dataset failed") 
