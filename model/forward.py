a08a7185bfd9c1bdfd1f7a5d9d88e677990906f1
vlozg <vulong61@gmail.com>
2022-05-22 15:41:56 +0700

    Update inference code to match with template

--- model/forward.py
+++ model/forward.py
@@ -30,3 +30,25 @@ def train_forward(model, embedder, batch, criterion, device):
     loss = criterion(mask, mixed_stft, target_stft)
     
     return output, mask, loss
+
+def inference_forward(model, embedder, batch, device):
+    dvec_mels = batch["dvec"]
+    mixed_stft = batch["mixed_stft"]
+
+    # Move to cuda
+    if device == "cuda":
+        mixed_stft = mixed_stft.cuda(non_blocking=True)
+        dvec_mels = [mel.cuda(non_blocking=True) for mel in dvec_mels]
+
+    # Get dvec
+    dvec_list = list()
+    for mel in dvec_mels:
+        dvec = embedder(mel)
+        dvec_list.append(dvec)
+    dvec = torch.stack(dvec_list, dim=0)
+    dvec = dvec.detach()
+
+    mask = model(torch.pow(mixed_stft.abs(), 0.3), dvec)
+    output = mixed_stft*torch.pow(mask, 10/3)
+    
+    return output, mask
