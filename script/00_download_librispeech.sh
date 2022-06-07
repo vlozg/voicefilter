65aba009c911dc44760182e4af4c64241bdc872b
vlozg <vulong61@gmail.com>
2022-06-07 03:00:59 +0000

    Fix script bug

--- script/00_download_librispeech.sh
+++ script/00_download_librispeech.sh
@@ -2,13 +2,13 @@ cd ../datasets
 
 # Download, then extract LibriSpeech datasets
 (axel -n 10 -q -c "https://www.openslr.org/resources/12/train-clean-100.tar.gz" \
-    && { tar -xzf train-clean-100.tar.gz && rm train-clean-100.tar.gz; }
+    && { tar -xzf train-clean-100.tar.gz && rm train-clean-100.tar.gz; } \
     || echo "Download train-clean-100 failed") &
 (axel -n 10 -q -c "https://www.openslr.org/resources/12/train-clean-360.tar.gz" \
     && { tar -xzf train-clean-360.tar.gz && rm train-clean-360.tar.gz; } \
     || echo "Download train-clean-360 failed") &
 (axel -n 10 -q -c "https://www.openslr.org/resources/12/dev-clean.tar.gz" \
-    && { tar -xzf dev-clean.tar.gz && rm dev-clean.tar.gz; }
+    && { tar -xzf dev-clean.tar.gz && rm dev-clean.tar.gz; } \
     || echo "Download dev-clean failed") &
 (axel -n 10 -q -c "https://www.openslr.org/resources/12/test-clean.tar.gz" \
     && { tar -xzf test-clean.tar.gz && rm test-clean.tar.gz; } \
