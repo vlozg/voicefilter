f5148b259d82273a804b8cd4e935189ee562311c
vlozg <vulong61@gmail.com>
2022-06-09 13:19:48 +0000

    Update inference: new forward, batch inference

--- inference.py
+++ inference.py
@@ -7,22 +7,28 @@ import numpy as np
 from utils.audio import Audio
 from utils.hparams import HParam
 
-from model.get_model import get_vfmodel, get_embedder
-from model.forward import inference_forward
+from model.get_model import get_vfmodel, get_embedder, get_forward
 from loss.get_criterion import get_criterion
 import soundfile
+from tqdm import tqdm
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--config', type=str, required=True,
                         help="yaml file for configuration")
-    parser.add_argument('-m', '--mixed_file', type=str, required=True,
+    parser.add_argument('-p', '--chkpt', type=str, required=True,
+                        help="path to pre-trained VF model checkpoint")
+    parser.add_argument('--use_cuda', type=bool, default=None,
+                        help="use cuda for testing, overwrite test config. Default: follow test config file")
+    parser.add_argument('-m', '--mixed_path', type=str, required=True,
                         help='path of mixed wav file')
-    parser.add_argument('-r', '--reference_file', type=str, required=True,
+    parser.add_argument('-r', '--reference_path', type=str, required=True,
                         help='path of reference wav file')
-    parser.add_argument('-o', '--out_file', type=str, required=True,
+    parser.add_argument('-o', '--out_path', type=str, required=True,
                         help='path of file to output')
+    parser.add_argument('--is_directory', type=bool, default=False,
+                        help="path above is directory of file. Default: False")
     args = parser.parse_args()
 
     config = HParam(args.config)["experiment"]
@@ -31,32 +37,45 @@ if __name__ == '__main__':
     device = "cuda" if config.use_cuda else "cpu"
     embedder = get_embedder(config, train=False, device=device)
     model, chkpt = get_vfmodel(config, train=False, device=device)
+    _, inference_forward = get_forward(config)
     criterion = get_criterion(config, reduction="none")
 
+    if args.is_directory:
+        print("Warning: using directory inference, audio in these folder must have same name as their counterpart")
+        file_list = [x.split("/")[-1] for x in glob.glob(os.path.join(args.mixed_path, '**', "*.wav"), recursive=True)]
+        mixed_list = [os.path.join(args.mixed_path, x) for x in file_list]
+        reference_list = [os.path.join(args.reference_path, x) for x in file_list]
+        out_list = [os.path.join(args.out_path, x) for x in file_list]
+    else:
+        mixed_list = [args.mixed_path]
+        reference_list = [args._reference_path]
+        out_list = [args.out_path]
 
-    with torch.no_grad():
-        d, _ = librosa.load(args.reference_file, sr=config.audio.sample_rate)
-        mixed_wav, _ = librosa.load(args.mixed_file, sr=config.audio.sample_rate)
 
-        norm = np.max(np.abs(mixed_wav)) * 1.1
-        mixed_wav = mixed_wav/norm
+    for mixed_file, reference_file, out_file in tqdm(zip(mixed_list, reference_list, out_list)):
+        with torch.no_grad():
+            d, _ = librosa.load(reference_file, sr=config.audio.sample_rate)
+            mixed_wav, _ = librosa.load(mixed_file, sr=config.audio.sample_rate)
 
-        dvec_mel = audio.get_mel(d)
-        dvec_mel = torch.from_numpy(dvec_mel).float()
-        mixed_stft = audio.stft(mixed_wav).T
-        mixed_stft = torch.from_numpy(mixed_stft)
+            norm = np.max(np.abs(mixed_wav)) * 1.1
+            mixed_wav = mixed_wav/norm
 
-        batch = {
-            "dvec": dvec_mel.unsqueeze(0),
-            "mixed_stft": mixed_stft.unsqueeze(0)
-        }
+            dvec_mel = audio.get_mel(d)
+            dvec_mel = torch.from_numpy(dvec_mel).float()
+            mixed_stft = audio.stft(mixed_wav).T
+            mixed_stft = torch.from_numpy(mixed_stft)
 
-        est_stft, est_mask = inference_forward(model, embedder, batch, device)
-        est_stft = est_stft.cpu().detach().numpy()
-        est_wav = audio._istft(est_stft[0].T, length=len(mixed_wav))
-    
-    print( est_wav.shape)
-    print( mixed_wav.shape)
-    out_dir = os.path.dirname(args.out_file)
-    os.makedirs(out_dir if out_dir != '' else ".", exist_ok=True)
-    soundfile.write(args.out_file, est_wav, config.audio.sample_rate)
+            batch = {
+                "dvec": dvec_mel.unsqueeze(0),
+                "dvec_wav": [d],
+                "mixed_stft": mixed_stft.unsqueeze(0),
+                "mixed_wav": torch.from_numpy(mixed_wav).unsqueeze(0)
+            }
+
+            est_stft, est_mask = inference_forward(model, embedder, batch, device)
+            est_stft = est_stft.cpu().detach().numpy()
+            est_wav = audio._istft(est_stft[0].T, length=len(mixed_wav))
+        
+        out_dir = os.path.dirname(out_file)
+        os.makedirs(out_dir if out_dir != '' else ".", exist_ok=True)
+        soundfile.write(out_file, est_wav, config.audio.sample_rate)
