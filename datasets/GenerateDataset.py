66eddc0c38f90a06e32b4dda0c2a5ddb74763cec
vlozg <vulong61@gmail.com>
2022-06-21 06:26:35 +0000

    Multiprocessing when generate df -> huge speed up

--- datasets/GenerateDataset.py
+++ datasets/GenerateDataset.py
@@ -1,5 +1,6 @@
 import warnings
 import random
+import concurrent.futures
 
 import torch
 import librosa
@@ -125,6 +126,47 @@ class VFDataset(Dataset):
         
         return features_dict
 
+
+def generate_sample(s1_target, s2, s1_dvec, sr, audio_len, min_dvec_len):
+    d, _ = librosa.load(s1_dvec, sr=sr)
+    w1, _ = librosa.load(s1_target, sr=sr)
+    w2, _ = librosa.load(s2, sr=sr)
+    assert len(d.shape) == len(w1.shape) == len(w2.shape) == 1, \
+        'wav files must be mono, not stereo'
+
+    # if reference for d-vector is too short, discard it
+    if d.shape[0] < min_dvec_len:
+        return
+
+    # I think random segment length will be better, but let's follow the paper first
+    # fit audio to `hp.data.audio_len` seconds.
+    # if merged audio is shorter than `L`, discard it
+    if w1.shape[0] < audio_len or w2.shape[0] < audio_len:
+        return
+
+    # Random 2 segment
+    l1 = random.randint(0, w1.shape[0]-audio_len)
+    l2 = random.randint(0, w2.shape[0]-audio_len)
+
+    # Post check if data sample is qualify
+    if audio_len > 0:
+        w1, w2 = w1[l1:l1+audio_len], w2[l2:l2+audio_len]
+    else:
+        w1, w2 = w1[l1:], w2[l2:]
+
+    # Discard almost silent target audio sample (since it will cause error in torch_mir_eval)
+    if w1.sum() < 10e-5:
+        return
+
+    return {
+        'clean_utterance_path': s1_target
+        ,'embedding_utterance_path': s1_dvec
+        ,'interference_utterance_path':s2
+        ,'clean_segment_start':l1
+        ,'interference_segment_start':l2
+    }
+    
+
 def generate_dataset_df(exp_config, dataset_config, speakers):
     config = exp_config
     sr = config.audio.sample_rate
@@ -140,7 +182,6 @@ def generate_dataset_df(exp_config, dataset_config, speakers):
     l1s = list()
     l2s = list()
 
-
     print('Generating ',dataset_config.detail[0],' dataset (save in file',dataset_config.file_path,'):')
     i=0
     pbar = tqdm.tqdm(total = dataset_config.size)
@@ -148,65 +189,58 @@ def generate_dataset_df(exp_config, dataset_config, speakers):
     dataset_detail = dataset_config.detail
     dataset_weight = dataset_config.get("weight")
 
-    while i<dataset_config.size:
-        # Random datasets to get speaker
-        if dataset_config.get("type") == "mixed" or dataset_config.get("type") is None:
-            dataset_choice = np.random.choice(dataset_detail, 2, replace=True, p=dataset_weight)
-        elif dataset_config.type == "cross":
-            dataset_choice = np.random.choice(dataset_detail, 1, replace=True, p=dataset_weight)
-            dataset_choice = np.repeat(dataset_choice, 2)
+    min_dvec_len = 1.1 * config.embedder.window * config.audio.hop_length
 
-        # Random 2 speakers from selected datasets
-        if dataset_choice[0] == dataset_choice[1]:
-            spk1, spk2 = random.sample(speakers[dataset_choice[0]], 2)
-        else:
-            spk1 = random.choice(speakers[dataset_choice[0]])
-            spk2 = random.choice(speakers[dataset_choice[1]])
-        s1_dvec, s1_target = random.sample(spk1, 2)
-        s2 = random.choice(spk2)
+    ###
+    # Start multiprocess generate
+    ###
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        future_samples = []
+        counter = 0
 
-        d, _ = librosa.load(s1_dvec, sr=sr)
-        w1, _ = librosa.load(s1_target, sr=sr)
-        w2, _ = librosa.load(s2, sr=sr)
-        assert len(d.shape) == len(w1.shape) == len(w2.shape) == 1, \
-            'wav files must be mono, not stereo'
+        while i<dataset_config.size:
+            # Random datasets to get speaker
+            if dataset_config.get("type") == "mixed" or dataset_config.get("type") is None:
+                dataset_choice = np.random.choice(dataset_detail, 2, replace=True, p=dataset_weight)
+            elif dataset_config.type == "cross":
+                dataset_choice = np.random.choice(dataset_detail, 1, replace=True, p=dataset_weight)
+                dataset_choice = np.repeat(dataset_choice, 2)
 
+            # Random 2 speakers from selected datasets
+            if dataset_choice[0] == dataset_choice[1]:
+                spk1, spk2 = random.sample(speakers[dataset_choice[0]], 2)
+            else:
+                spk1 = random.choice(speakers[dataset_choice[0]])
+                spk2 = random.choice(speakers[dataset_choice[1]])
+            s1_dvec, s1_target = random.sample(spk1, 2)
+            s2 = random.choice(spk2)
 
-        # if reference for d-vector is too short, discard it
-        if d.shape[0] < 1.1 * config.embedder.window * config.audio.hop_length:
-            continue
+            future_samples.append(executor.submit(generate_sample, s1_target, s2, s1_dvec, sr, audio_len, min_dvec_len))
+            counter += 1
 
-        # I think random segment length will be better, but let's follow the paper first
-        # fit audio to `hp.data.audio_len` seconds.
-        # if merged audio is shorter than `L`, discard it
-        if w1.shape[0] < audio_len or w2.shape[0] < audio_len:
-            continue
+            if counter == 100:
+                for future in concurrent.futures.as_completed(future_samples):
+                    try:
+                        data = future.result()
+                    except Exception as exc:
+                        print('Generated an exception: %s' % (exc))
+                    else:
+                        if data is None: continue
 
-        # Random 2 segment
-        l1 = random.randint(0, w1.shape[0]-audio_len)
-        l2 = random.randint(0, w2.shape[0]-audio_len)
+                        s1_targets.append(data["clean_utterance_path"])
+                        s1_dvecs.append(data["embedding_utterance_path"])
+                        s2s.append(data["interference_utterance_path"])
+                        l1s.append(data["clean_segment_start"])
+                        l2s.append(data["interference_segment_start"])
+                        i += 1
+                        pbar.update(1)
 
-        # Post check if data sample is qualify
-        if audio_len > 0:
-            w1, w2 = w1[l1:l1+audio_len], w2[l2:l2+audio_len]
-        else:
-            w1, w2 = w1[l1:], w2[l2:]
+                        if i==dataset_config.size: break
 
-        # Discard almost silent target audio sample (since it will cause error in torch_mir_eval)
-        if w1.sum() < 10e-5:
-            continue
+                counter = 0
 
-        s1_targets.append(s1_target)
-        s1_dvecs.append(s1_dvec)
-        s2s.append(s2)
-        l1s.append(l1)
-        l2s.append(l2)
+        pbar.close()
 
-        i += 1
-        pbar.update(1)
-
-    pbar.close()
-    
     df=pd.DataFrame(
         {'clean_utterance_path': s1_targets
         ,'embedding_utterance_path': s1_dvecs
